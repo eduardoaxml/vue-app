c3a87d35c2e57f97610cfbe26ca8bb6fc5a0319f
Pablo Eduardo Merino <3802987+eduardoaxml@users.noreply.github.com>
2022-08-08 11:53:54 -0500

    Components & Props

--- main.js
+++ main.js
@@ -1,55 +1,9 @@
 const app = Vue.createApp({
-    data: function() {
+    data() {
         return {
             cart: 0,
-            product: 'Socks',
-            brand: 'Vue Mastery',
-            selectedVariant: 0,
-            description: 'This is a little description of the product',
-            image: './assets/images/socks_green.jpg',
-            price: '$10',
-            url: 'https://www.google.com',
-            inStock: false,
-            inventory: 0,
-            onSale: false,
-            details: ['80% cotton', '20% wool', '50% polyester'],
-            sizes: ['S', 'M', 'L', 'XL'],
-            variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]
+            premium: true
         }
     },
-    methods: {
-        addToCart() {
-            this.cart += 1
-        },
-        // solution
-        removeFromCart() {
-            if (this.cart >= 1) {
-                this.cart -= 1
-            }
-        },
-
-        updateImage(variantImage) {
-            this.image = variantImage
-        }
-    },
-    computed: {
-        title() {
-            return this.brand + ' ' + this.product
-        },
-        image() {
-            return this.variants[this.selectedVariant].image
-        },
-        inStock() {
-            return this.variants[this.selectedVariant].quantity
-        },
-        saleMessage() {
-            if (this.onSale) {
-                return this.brand + ' ' + this.product + ' is on sale.'
-            }
-            return ''
-        }
-    }
-});
+    methods: {}
+})
